7d9e5e7c39bf6e0e9325c35629ad054f70f8a9fb
Matt Ridgway <matt.ridgway@zupa.co.uk>
2020-07-11 00:24:23 +0100

    Added CQRS library

---
+++ lib/Stark.CQRS/Aggregate.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stark.CQRS
+{
+    public abstract class Aggregate
+    {
+        public abstract Guid Id { get; }
+
+        private const long _newAggregateVersions = -1;
+        protected long Version { get; set; } = _newAggregateVersions;
+    }
+}
