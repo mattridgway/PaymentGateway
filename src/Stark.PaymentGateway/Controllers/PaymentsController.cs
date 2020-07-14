1bb72a57320f50bfa54aad915c6b6ea9c9884990
Matt Ridgway <matt.ridgway@zupa.co.uk>
2020-07-14 20:56:03 +0100

    Added new endpoint for retrieving payment details

---
+++ src/Stark.PaymentGateway/Controllers/PaymentsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Stark.PaymentGateway.Application.Payments.Queries;
+using System;
+using System.Threading.Tasks;
+
+namespace Stark.PaymentGateway.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/payments")]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IReadPaymentDetailProjections _repository;
+
+        public PaymentsController(IReadPaymentDetailProjections repository)
+        {
+            _repository = repository;
+        }
+
+        [ProducesResponseType(200)]
+        [ProducesResponseType(404)]
+        [HttpGet("{id}", Name = "Retrieve payment details")]
+        public async Task<ActionResult<PaymentDetails>> RetrievePayment(Guid id)
+        {
+            var payment = await _repository.GetPaymentByIdAsync(id, "merchant");
+
+            if (payment is null)
+                return NotFound();
+
+            return Ok(payment);
+        }
+    }
+}
---
+++ src/Stark.PaymentGateway/PaymentDetails.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Stark.PaymentGateway
+{
+    public class PaymentDetails
+    {
+        public Guid Id { get; set; }
+        public bool? IsSuccess { get; set; }
+        public string MaskedCardNumber { get; set; }
+        public double Amount { get; set; }
+        public string Currency { get; set; }
+    }
+}
