95bd6fe4a102e5c0a9d47544d39e9c822ea39cdc
Matt Ridgway <matt.ridgway@zupa.co.uk>
2020-07-16 19:53:11 +0100

    Tidied code and updated README

--- src/Stark.PaymentGateway/Startup.cs
+++ src/Stark.PaymentGateway/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Stark.PaymentGateway.Application;
 using Stark.PaymentGateway.Infrastructure;
+using System;
 
 namespace Stark.PaymentGateway
 {
